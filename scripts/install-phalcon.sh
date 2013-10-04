90910ec07130ab95ff755dbc68b047468848a927
Rack Lin <racklin@gmail.com>
2013-10-04 10:59:37 +0800

    Added phalcon-devtools Signed-off-by: Rack Lin <racklin@gmail.com>

--- scripts/install-phalcon.sh
+++ scripts/install-phalcon.sh
@@ -10,5 +10,21 @@ sudo ./install
 
 sudo cp -f /vagrant/files/etc/php5/conf.d/phalcon.ini /etc/php5/conf.d/
 
+sudo chmod -R vagrant:vagrant /home/vagrant/cphalcon
+
 # restart php5-fpm
 /etc/init.d/php5-fpm restart
+
+
+#
+# phalcon developer tools
+#
+mkdir /home/vagrant/phalcon-devtools
+cd /home/vagrant/phalcon-devtools
+echo '{ "require": { "phalcon/devtools": "dev-master" } }' > composer.json
+/home/vagrant/bin/composer install
+
+ln -s /home/vagrant/phalcon-devtools/vendor/phalcon/devtools/phalcon.php /home/vagrant/bin/phalcon
+
+sudo chmod -R vagrant:vagrant /home/vagrant/phalcon-devtools
+sudo chmod -R vagrant:vagrant /home/vagrant/bin
